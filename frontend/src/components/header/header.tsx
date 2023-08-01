f45ee11832280055b50e28c291afb2fbf0c5e067
leandrobalta <leandrobraga.job@gmail.com>
2023-08-01 00:50:35 -0300

    finished hero, have a lot of things to upgrade

--- frontend/src/components/header/header.tsx
+++ frontend/src/components/header/header.tsx
@@ -15,7 +15,7 @@ const MenuDropdownTitle = () => (
 
 export function Header() {
     return (
-        <Navbar expand="lg" className="bg-body-tertiary">
+        <Navbar expand="lg" className="bg-body-tertiary" id="navbar">
             <Container>
                 <Navbar.Brand href="#home">
                     <CavokTextIcon id="cavok-text-icon" />
@@ -25,11 +25,17 @@ export function Header() {
                     <Nav.Link className="text-colored" href="#about">Fale Conosco</Nav.Link>
                     <NavDropdown title={ <MenuDropdownTitle/> } id="menu-dropdown">
                         <NavDropdown.Item href="#action/3.1">Usuário</NavDropdown.Item>
+                        <NavDropdown.Divider />
                         <NavDropdown.Item href="#action/3.2">Meus Pedidos</NavDropdown.Item>
+                        <NavDropdown.Divider />
                         <NavDropdown.Item href="#action/3.3">Cancelamento/Reembolso</NavDropdown.Item>
+                        <NavDropdown.Divider />
                         <NavDropdown.Item href="#action/3.4">Termos e Condições</NavDropdown.Item>
+                        <NavDropdown.Divider />
                         <NavDropdown.Item href="#action/3.1">Central de Ajuda</NavDropdown.Item>
+                        <NavDropdown.Divider />
                         <NavDropdown.Item href="#action/3.1">Política de Privacidade</NavDropdown.Item>
+                        <NavDropdown.Divider />
                         <NavDropdown.Item href="#action/3.1">Dúdivas Frequentes</NavDropdown.Item>
                     </NavDropdown>
                 </Nav>
--- frontend/src/components/home-hero/hero.tsx
+++ frontend/src/components/home-hero/hero.tsx
@@ -1,13 +1,86 @@
-import "./hero.css"
+import { useState } from "react";
+import "./hero.css";
+import { Form, ButtonGroup, ToggleButton, Button } from "react-bootstrap";
+import styled from "styled-components";
+
+const ToggleButtonStyled = styled(ToggleButton)<{$checked?: boolean}>`
+    border: 1px solid #134074;
+    color: #fff;
+    color: ${props => props.$checked ? "#fff" : "#134074"};
+    background-color: ${props => props.$checked ? "#134074" : "#fff"};
+    &:hover {
+        background-color: ${props => props.$checked ? "#134074" : "#fff"};
+        color: ${props => props.$checked ? "#fff" : "#134074"};
+    }
+
+`
+
+const SearchButton = styled(Button)`
+    background-color: #134074;
+    color: #fff;
+    border: 1px solid #134074;
+    &:hover {
+        background-color: #134085;
+        border: 1px solid #134085;
+    }
+`
 
 export default function Hero() {
+    const [travelModeValue, setTravelModeValue] = useState("round-trip");
+
+    const travelMode = [
+        { name: "Ida e volta", value: "round-trip" },
+        { name: "Somente ida", value: "one-way" },
+    ];
 
     return (
         <div className="hero">
             {/* <video src={video} muted autoPlay loop typeof="video/mp4" ></video> */}
-            <div className="content">
-                <h2>PASSAGENS AÉREAS</h2>
+            <div className="conteiner">
+                <div className="row">
+                    <h3>PASSAGENS AÉREAS</h3>
+                    <ButtonGroup >
+                        {travelMode.map((travelMode, idx) => (
+                            <ToggleButtonStyled
+                                $checked={travelModeValue === travelMode.value}
+                                // style={{ border: "1px solid #134074", color: "#fff", backgroundColor: "#134074" }}
+                                key={idx}
+                                color="#134074"
+                                id={`radio-${idx}`}
+                                type="radio"
+                                // variant="outline-primary"
+                                name="radio"
+                                value={travelMode.value}
+                                checked={travelModeValue === travelMode.value}
+                                onChange={(e) => setTravelModeValue(e.currentTarget.value)}
+                            >
+                                {travelMode.name}
+                            </ToggleButtonStyled>
+                            
+                        ))}
+                    </ButtonGroup>
+                    <p></p>
+                    <p></p>
+                </div>
+                <br />
+                <div className="row">
+                    <Form.Group className="location-input">
+                        <Form.Label>Origem</Form.Label>
+                        <Form.Control className="hero-input" type="text" placeholder="Digite a cidade de origem" />
+                    </Form.Group>
+                    <Form.Group className="location-input">
+                        <Form.Label>Destino</Form.Label>
+                        <Form.Control type="text" placeholder="Digite a cidade de origem" />
+                    </Form.Group>
+                    <Form.Group className="location-input">
+                        <Form.Label>Data de ida</Form.Label>
+                        <Form.Control type="date" placeholder="Digite a cidade de origem" />
+                    </Form.Group>
+                    <SearchButton id="search-btn">
+                        BUSCAR VOOS
+                    </SearchButton>
+                </div>
             </div>
         </div>
-    )
+    );
 }
