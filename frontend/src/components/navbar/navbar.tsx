c47a12b2d635a99e747cc8c4f0b627aa56bc1493
leandrobalta <leandrobraga.job@gmail.com>
2023-08-08 23:42:49 -0300

    added my menu button, need complete and make responsive

--- frontend/src/components/navbar/navbar.tsx
+++ frontend/src/components/navbar/navbar.tsx
@@ -1,8 +1,17 @@
 import "./navbar.css";
 import { AiOutlineMenu as MenuIcon } from "react-icons/ai";
 import CavokTextIcon from "components/icons/cavok-text";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import { useState } from "react";
 
 export function NavBar() {
+    const [showMenuBtn, setshowMenuBtn] = useState(false);
+
+    const toggleDropdown = () => {
+        setshowMenuBtn(!showMenuBtn);
+    };
+
     return (
         <nav className="navbar">
             <div className="brand">
@@ -21,9 +30,20 @@ export function NavBar() {
                         </li>
                     </ul>
                 </div>
-                <button className="menu-btn">
-                    <MenuIcon size={25}/>
-                </button>
+                <div className="dropdown">
+                    <button className="menu-btn" onClick={(evt) => toggleDropdown()}>
+                        <MenuIcon size={25} />
+                    </button>
+                    {
+                        showMenuBtn && (
+                            <div className="dropdown-content">
+                                <a href="">Option 1</a>
+                                <hr style={{ margin: "0" }}/>
+                                <a href="">Option 2</a>
+                            </div>
+                        )
+                    }
+                </div>
             </div>
         </nav>
     );
