ad62a0daf839ddfc1777e5891f12f3207518e065
leandrobalta <leandrobraga.job@gmail.com>
2023-11-16 01:58:14 -0300

    added a sample of an ASP.NET api server

---
+++ server/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace server.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Updated { get; set; }
+    }
+}
+
